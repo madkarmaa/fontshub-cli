cbb9154b35a0591bd88c1ef0ca3fcc6650aaf293
madkarmaa <madkarma99@gmail.com>
2025-12-16 10:20:18 +0100

    refactor: format

--- src/http.ts
+++ src/http.ts
@@ -5,7 +5,9 @@ export const fetchFonts = async () => {
     const res = await fetch(URL);
 
     if (!res.ok)
-        throw new Error(`Failed to fetch fonts (${res.status})\n${res.statusText}`);
+        throw new Error(
+            `Failed to fetch fonts (${res.status})\n${res.statusText}`,
+        );
 
     const data = await res.json();
     return ResponseSchema.parse(data);
