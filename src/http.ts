1d1f34f3457651724026376cdf0995157f990b81
madkarmaa <madkarma99@gmail.com>
2025-12-16 10:20:08 +0100

    refactor: remove axios

--- src/http.ts
+++ src/http.ts
@@ -1,8 +1,12 @@
-import axios from 'axios';
 import { ResponseSchema } from './schemas';
 import { URL } from './utils';
 
 export const fetchFonts = async () => {
-    const res = await axios.get(URL);
-    return ResponseSchema.parse(res.data);
+    const res = await fetch(URL);
+
+    if (!res.ok)
+        throw new Error(`Failed to fetch fonts (${res.status})\n${res.statusText}`);
+
+    const data = await res.json();
+    return ResponseSchema.parse(data);
 };
